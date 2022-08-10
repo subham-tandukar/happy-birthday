baca0cef733e17702415ed7f8e8ec27ac9f2548c
Subham Tandukar <subhamtndkr9@gmail.com>
2022-08-10 12:57:14 +0545

    commit

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,19 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const HeroBanner = () => {
   return (
     <>
-      <div className='hero-banner'>
-      <div className="uk-inline uk-width-1">
-            <img src="images/img1.jpg" alt=""/>
-            <div className="uk-overlay-primary uk-position-cover"></div>
-            <div className="uk-overlay text uk-light">
-                <h1>Shoutout To Your Mom And Dad <br /> For Making You 🔊</h1>
-            </div>
+      <div className="hero-banner">
+        <div className="uk-inline uk-width-1">
+          <img src="images/img1.jpg" alt="" />
+          <div className="uk-overlay-primary uk-position-cover"></div>
+          <div className="uk-overlay text uk-light">
+            <h1>
+              Shoutout To Your Mom And Dad <br /> For Making You 🔊
+            </h1>
+          </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default HeroBanner
+export default HeroBanner;
